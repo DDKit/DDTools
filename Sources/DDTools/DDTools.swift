be37cb601107be4a36f60c9c09dd46bce87f8af5
DuanChangHe <duanchanghe@gmail.com>
2019-10-10 10:10:37 +0800

    0.0.3

--- Sources/DDTools/DDTools.swift
+++ Sources/DDTools/DDTools.swift
@@ -1,3 +1,21 @@
-public struct DDTools {
-    public var text = "Hello, World!"
+import Foundation
+
+open struct DDTools {
+    var text = "Hello, World!"
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
+
+
+open class Person: NSObject {
+    
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    open func adds(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
 }
